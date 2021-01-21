4071615726b19f2ba2ca15f2df361ca03e2489a5
vivntng <vivntng@users.noreply.github.com>
2021-01-21 10:36:59 -0800

    lab3 completion

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,13 +5,35 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+function projectClick(e) {
+    e.preventDefault();
+	//$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if(description.length == 0){
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(description).fadeToggle();
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+	//containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Ask your own heart.");
+		//$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Please wait...");
 	});
+
+    $("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
